update segrnn, micn, dlinear. fix tsr normalization

run.py
@@ -35,8 +35,11 @@ def main(args):
     parent_seed = args.seed
     np.random.seed(parent_seed)
     experiment_seeds = np.random.randint(1e3, size=args.itrs)
-    
+    original_itr = args.itr_no
+        
     for itr_no in range(1, args.itrs+1):
+        if (original_itr is not None) and original_itr != itr_no: continue
+        
         args.seed = experiment_seeds[itr_no-1]
         print(f'\n>>>> itr_no: {itr_no}, seed: {args.seed} <<<<<<')
         set_random_seed(args.seed)

utils/tsr_tunnel.py
@@ -17,7 +17,7 @@ from captum._utils.typing import (
 
 from torch import Tensor
 from typing import Any, Callable, Tuple, Union
-
+from utils.tools import min_max_scale
 from tint.attr.occlusion import FeatureAblation, Occlusion
 
 
@@ -291,16 +291,17 @@ class WTSR(Occlusion):
 
         # Normalize if required
         if normalize:
+            # min max scaling
             time_relevance_score = tuple(
-                tsr / tsr.sum() for tsr in time_relevance_score
+                min_max_scale(tsr) for tsr in time_relevance_score
             )
-
+            
         # Get indexes where the Time-Relevance Score is
         # higher than the threshold
         is_above_threshold = tuple(
-            score > threshold for score in time_relevance_score
+            score > torch.quantile(score, threshold, dim=1, keepdim=True) for score in time_relevance_score
         )
-
+        
         # Formatting strides
         strides = _format_and_verify_strides(strides, inputs)
 
@@ -532,5 +533,6 @@ class WTSR(Occlusion):
         # For this method, we need to reshape eval_diff to the output shapes
         return eval_diff.reshape((len(eval_diff),) + shapes)
     
-    def get_name(self):
-        return f'WTSR + {self.attribution_method.get_name()}'
+    @staticmethod
+    def get_name():
+        return 'WTSR'

utils/winIT.py
@@ -108,7 +108,7 @@ class WinIT:
             
             for t in range(seq_len)[::-1]:
                 # mask last t timesteps
-                inputs_hat[:, t:, feature] = counterfactuals[:, t:]
+                inputs_hat[:, t, feature] = counterfactuals[:, t]
             
                 if type(additional_forward_args) == tuple:
                     y_perturbed = self.format_output(
@@ -122,8 +122,9 @@ class WinIT:
                 iSab = self._compute_metric(y_original, y_perturbed)
                 iSab = torch.clip(iSab, -1e6, 1e6)
                 iS_array[:, :, t, feature] = iSab
-                    
-        iS_array[:, :, 1:] -= iS_array[:, :, :-1]
+        
+        # i(S, a, b) i(S)^b_a − i(S)^b_a+1
+        iS_array[:, :, :-1] -= iS_array[:, :, 1:] 
         
         if attributions_fn is not None:
             attr = attributions_fn(iS_array)
@@ -175,7 +176,7 @@ class WinIT:
                 
                 for t in range(seq_len)[::-1]:
                     # mask last t timesteps
-                    cloned[:, t:, feature] = counterfactuals[:, t:]
+                    cloned[:, t, feature] = counterfactuals[:, t]
                     
                     inputs_hat = []
                     for i in range(len(inputs)):
@@ -195,7 +196,7 @@ class WinIT:
                     iSab = torch.clip(iSab, -1e6, 1e6)
                     iS_array[:, :, t, feature] = iSab
                         
-            iS_array[:, :, 1:] -= iS_array[:, :, :-1]
+            iS_array[:, :, :-1] -= iS_array[:, :, 1:]
             
             if attributions_fn is not None:
                 attr.append(attributions_fn(iS_array))
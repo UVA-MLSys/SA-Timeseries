fix normalization

utils/WinTSR.py
@@ -20,7 +20,7 @@ from typing import Any, Callable, Tuple, Union
 from utils.tools import normalize_scale, reshape_over_output_horizon
 from tint.attr.occlusion import FeatureAblation, Occlusion
 
-class TSR(Occlusion):
+class WinTSR(Occlusion):
     r"""
     Two-step temporal saliency rescaling Tunnel.
     Performs a two-step interpretation method:
@@ -262,7 +262,7 @@ class TSR(Occlusion):
             (1,) + input.shape[2:] for input in inputs
         )
 
-        # Compute the Time-Relevance Score (step 1)
+        # Compute the Time-Relevance Score (step 1). Shape ((N x O) x seq_len x features)
         time_relevance_score = super().attribute.__wrapped__(
             self,
             inputs=inputs,
@@ -277,30 +277,46 @@ class TSR(Occlusion):
             #TODO: uncomment after new release
             # kwargs_run_forward=kwargs,
         )
-
+        
         # Reshape the Time-Relevance Score and sum along the diagonal
-        assert all(
-            tsr.shape == input.shape
-            for input, tsr in zip(inputs, time_relevance_score)
-        ), "The attribution method must return a tensor of the same shape as the inputs"
+        # assert all(
+        #     tsr.shape == input.shape
+        #     for input, tsr in zip(inputs, time_relevance_score)
+        # ), "The attribution method must return a tensor of the same shape as the inputs"
         time_relevance_score = tuple(
             tsr.sum((tuple(i for i in range(2, len(tsr.shape)))))
             for tsr in time_relevance_score
         )
 
+        if target is None:
+            # time_relevance_score shape will be ((N x O) x seq_len) after previous summation
+            # reshape it to be (N x O x seq_len)
+            time_relevance_score = tuple(
+                tsr.reshape((input.shape[0], -1) + input.shape[1:-1])
+                for input, tsr in zip(inputs, time_relevance_score)
+            )
+            # take avg over the output horizon
+            # now shape is (N x seq_len x features) which can now be passsed 
+            # as is_above_threshold to Feature Ablation
+            time_relevance_score = tuple(
+                tsr.mean(dim=1) for tsr in time_relevance_score
+            )
+            
+        print('tsr shape ', [tsr.shape for tsr in time_relevance_score])
+
         # Normalize if required
         if normalize:
-            # min max scaling
+            # normalize the last dimension
             time_relevance_score = tuple(
                 normalize_scale(
-                    tsr, dim=1, norm_type="l1"
+                    tsr, dim=-1, norm_type="l1"
                 ) for tsr in time_relevance_score
             )
             
         # Get indexes where the Time-Relevance Score is
         # higher than the threshold
         is_above_threshold = tuple(
-            score > torch.quantile(score, threshold, dim=1, keepdim=True) for score in time_relevance_score
+            score > torch.quantile(score, threshold, dim=-1, keepdim=True) for score in time_relevance_score
         )
         
         # Formatting strides
@@ -428,6 +444,8 @@ class TSR(Occlusion):
             ],
             dim=0,
         ).long()
+        print('Expanded input ', expanded_input.shape, input_mask.shape)
+        
         ablated_tensor = (
             expanded_input
             * (
@@ -505,7 +523,9 @@ class TSR(Occlusion):
                 ],
             ] = 0
 
-        return is_above.unsqueeze(-1) * padded_tensor.unsqueeze(0)
+        current_mask = is_above.unsqueeze(-1) * padded_tensor.unsqueeze(0)
+        print('Mask shape ', current_mask.shape)
+        return current_mask
 
     def _run_forward(
         self, forward_func: Callable, inputs: Any, **kwargs
@@ -536,4 +556,4 @@ class TSR(Occlusion):
     
     @staticmethod
     def get_name():
-        return 'TSR'
+        return 'WinTSR'

utils/explainer.py
@@ -1,4 +1,5 @@
 import torch
+from utils.tools import reshape_over_output_horizon
 
 def get_total_data(dataloader, device, add_x_mark=True):        
     if add_x_mark:
@@ -50,6 +51,16 @@ def compute_attr(
         ])
     else: sliding_window_shapes = (1,1)
     
+    # if name == 'wtsr':
+    #     attr = explainer.attribute(
+    #         inputs=inputs,
+    #         sliding_window_shapes=sliding_window_shapes,
+    #         baselines=baselines,
+    #         additional_forward_args=additional_forward_args,
+    #         threshold=0, normalize=True,
+    #         attributions_fn=abs
+    #     )
+    
     if name in [
         'deep_lift', 'lime', 'integrated_gradients', 
         'gradient_shap', 'tsr', 'wtsr'
@@ -58,7 +69,7 @@ def compute_attr(
         
         for target in range(targets):
             if name in ['tsr', 'wtsr']:
-                threshold = 0.55 if name == 'tsr' else 0
+                threshold = 0.55 # if name == 'tsr' else 0
                 attr = explainer.attribute(
                     inputs=inputs,
                     sliding_window_shapes=sliding_window_shapes,
@@ -88,7 +99,14 @@ def compute_attr(
             # pred_len x batch x seq_len x features -> batch x pred_len x seq_len x features
             attr = attr.permute(1, 0, 2, 3)
         
-    elif name in ['feature_permutation', 'winIT', 'feature_ablation']:
+    elif name in ['feature_ablation']:
+        attr = explainer.attribute(
+            inputs=inputs, baselines=baselines, 
+            attributions_fn=abs,
+            additional_forward_args=additional_forward_args
+        )
+        
+    elif name in ['feature_permutation', 'winIT']:
         attr = explainer.attribute(
             inputs=inputs, attributions_fn=abs,
             additional_forward_args=additional_forward_args
@@ -116,26 +134,6 @@ def compute_attr(
     else:
         return reshape_over_output_horizon(attr, inputs, args)
 
-def reshape_over_output_horizon(attr, inputs, args):
-    if type(inputs) == tuple:
-        # tuple of batch x seq_len x features
-        attr = tuple([
-            attr_.reshape(
-                # batch x pred_len x seq_len x features
-                (inputs[0].shape[0], -1, args.seq_len, attr_.shape[-1])
-            # take mean over the output horizon
-            ) for attr_ in attr
-        ])
-    else:
-        # batch x seq_len x features
-        attr = attr.reshape(
-            # batch x pred_len x seq_len x features
-            (inputs.shape[0], -1, args.seq_len, attr.shape[-1])
-        # take mean over the output horizon
-        )
-    
-    return attr
-
 def avg_over_output_horizon(attr, inputs, args):
     if type(inputs) == tuple:
         # tuple of batch x seq_len x features
@@ -155,58 +153,3 @@ def avg_over_output_horizon(attr, inputs, args):
         ).mean(axis=1)
     
     return attr
-
-def compute_tsr_attr(
-    args, explainer, inputs, sliding_window_shapes, 
-    strides=None, baselines=None,
-    additional_forward_args=None, threshold=0.0, 
-    normalize=True, avg_attr=True
-):
-    attr_list = []
-    if args.task_name == 'classification':
-        targets = args.num_class
-    else:
-        targets = args.pred_len
-        
-    for target in range(targets):
-        score = explainer.attribute(
-            inputs=inputs, sliding_window_shapes=sliding_window_shapes,
-            strides=strides,
-            baselines=baselines, target=target,
-            additional_forward_args=additional_forward_args,
-            threshold=threshold, normalize=normalize
-        )
-        attr_list.append(score)
-        
-    if type(inputs) == tuple:
-            attr = []
-            for input_index in range(len(inputs)):
-                attr_per_input = torch.stack([score[input_index] for score in attr_list])
-                # targets x batch x seq_len x features -> batch x targets x seq_len x features
-                attr_per_input = attr_per_input.permute(1, 0, 2, 3)
-                attr.append(attr_per_input)
-                
-            attr = tuple(attr)
-    else:
-        attr = torch.stack(attr_list)
-        # targets x batch x seq_len x features -> batch x targets x seq_len x features
-        attr = attr.permute(1, 0, 2, 3)
-    
-    if avg_attr:
-        return avg_over_output_horizon(attr, inputs, args)
-    else:
-        return reshape_over_output_horizon(attr, inputs, args)
-
-def min_max_scale(arr, dim:int=0):
-    assert dim in [0, 1], f'Dimension must be 0 or 1, found {dim}'
-    mx, mn = torch.max(arr, dim=dim).values, torch.min(arr, dim=dim).values
-    denom = (mx - mn)
-    
-    if dim == 0: scaled = (arr - mn)/denom
-    else: scaled = ((arr.T - mn)/denom).T
-    
-    # replace nan values with 0
-    # possible when all values are same, hence denom is 0
-    scaled[scaled != scaled] = 0
-    
-    return scaled

utils/tools.py
@@ -1,11 +1,79 @@
-import os
-
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-import pandas as pd
+
+from captum._utils.typing import (
+    TensorOrTupleOfTensorsGeneric,
+)
 
 plt.switch_backend('agg')
+
+def reshape_over_output_horizon(attr, inputs, args):
+    if type(inputs) == tuple:
+        # tuple of batch x seq_len x features
+        attr = tuple([
+            attr_.reshape(
+                # batch x pred_len x seq_len x features
+                (inputs[0].shape[0], -1, args.seq_len, attr_.shape[-1])
+            # take mean over the output horizon
+            ) for attr_ in attr
+        ])
+    else:
+        # batch x seq_len x features
+        attr = attr.reshape(
+            # batch x pred_len x seq_len x features
+            (inputs.shape[0], -1, args.seq_len, attr.shape[-1])
+        # take mean over the output horizon
+        )
+    
+    return attr
+
+#TODO: debug error for some cases
+def reshape_attr(
+    attr: TensorOrTupleOfTensorsGeneric, 
+    inputs: TensorOrTupleOfTensorsGeneric
+):
+    sample_size = inputs[0].shape[0] if type(inputs) == tuple  else inputs.shape[0]
+    if type(attr) == tuple:
+        # tuple of batch x seq_len x features
+        attr = tuple([
+            attr_.reshape(
+                # batch x pred_len x seq_len x features
+                ((sample_size, -1) + attr_.shape[1:])
+            # take mean over the output horizon
+            ) for attr_ in attr
+        ])
+    else:
+        # batch x seq_len x features
+        attr = attr.reshape(
+            # batch x pred_len x seq_len x features
+            ((sample_size, -1) + (attr.shape[1:]))
+        # take mean over the output horizon
+        )
+    
+    return attr
+
+def normalize_scale(
+    data: torch.Tensor, dim=1,
+    norm_type='standard'
+):
+    if norm_type == "standard":
+        mean = data.mean(dim=dim, keepdim=True)
+        std = data.std(dim=dim, keepdim=True)
+        return (data - mean) / (std + torch.finfo(torch.float32).eps)
+
+    elif norm_type == "minmax":
+        max_val = torch.amax(data, dim=dim, keepdim=True)[0]
+        min_val = torch.amin(data, dim=dim, keepdim=True)[0]
+        return (data - min_val) / (max_val - min_val + torch.finfo(torch.float32).eps)
+        
+    elif norm_type == "l1":
+        sum_val = data.abs().sum(dim=dim, keepdim=True)
+        
+        # this converts neg to absolute values
+        return data.abs() / (sum_val + torch.finfo(torch.float32).eps)
+    else:
+        raise (NameError(f'Normalize method "{norm_type}" not implemented'))
 
 # min max scale a torch tensor across a dimension
 def min_max_scale(a:torch.Tensor, dim=1, absolute=True):

utils/winIT.py
@@ -1,13 +1,13 @@
-import torch
+import torch, gc
 import numpy as np
 from captum._utils.typing import (
     BaselineType,
     TargetType,
     TensorOrTupleOfTensorsGeneric,
 )
-from typing import Callable, Optional
+from typing import Callable, Optional, Tuple, Union
 
-
+# Source:https://github.com/layer6ai-labs/WinIT
 class WinIT:
     def __init__(self, model, data, args):
         self.model = model
@@ -48,11 +48,12 @@ class WinIT:
             rhs = torch.nn.KLDivLoss(reduction="none")(torch.log(average), p_y_exp)
             return torch.sum((lhs + rhs) / 2, -1)
         if self.metric == "pd":
+            # batch_size x output_horizon x num_states
             diff = torch.abs(p_y_hat - p_y_exp)
             
-            # sum over all dimension except batch
-            summed = torch.sum(diff, dim=-1) # tuple(range(diff.ndim)[1:])
-            return summed
+            # sum over all dimension except batch, output_horizon
+            # multioutput multi-horizon not supported yet
+            return torch.sum(diff, dim=-1)
         
         raise Exception(f"unknown metric. {self.metric}")
     
@@ -74,107 +75,35 @@ class WinIT:
             f_dim = -1 if self.args.features == 'MS' else 0
             outputs = outputs[:, -self.args.pred_len:, f_dim:]
             return outputs
-        
-    def _attribute(
-        self, inputs, additional_forward_args, 
-        attributions_fn=None
-    ):
-        model = self.model
-        
-        with torch.no_grad():
-            if type(additional_forward_args) == tuple:
-                y_original = self.format_output(
-                    model(inputs, *additional_forward_args)
-                )
-            else:
-                y_original = self.format_output(
-                    model(inputs, additional_forward_args)
-                )
-        
-        batch_size, seq_len, n_features = inputs.shape
-        
-        n_output = self.args.num_class if self.task_name == 'classification' else self.args.pred_len
-        iS_array = torch.zeros(size=(
-            batch_size, n_output, seq_len, n_features
-        ), device=inputs.device)
-        
-        with torch.no_grad():
-            for feature in range(n_features):
-                inputs_hat = inputs.clone()
-                counterfactuals = self.generate_counterfactuals(
-                    batch_size, None, feature
-                )
-                
-                for t in range(seq_len)[::-1]:
-                    # mask last t timesteps
-                    inputs_hat[:, t, feature] = counterfactuals[:, t]
-                
-                    if type(additional_forward_args) == tuple:
-                        y_perturbed = self.format_output(
-                            model(inputs_hat, *additional_forward_args)
-                        )
-                    else:
-                        y_perturbed = self.format_output(
-                            model(inputs_hat, additional_forward_args)
-                        )
-                    
-                    iSab = self._compute_metric(y_original, y_perturbed)
-                    iSab = torch.clip(iSab, -1e6, 1e6)
-                    iS_array[:, :, t, feature] = iSab
-                    
-                    del y_perturbed, iSab
-                del counterfactuals
-        
-        # i(S, a, b) i(S)^b_a − i(S)^b_a+1
-        iS_array[:, :, 1:] -= iS_array[:, :, :-1] 
-        
-        # reverse order along the time axis
-        # equivalent to iS_array[:, :, ::-1] which doesn't work 
-        # sometimes since torch doesn't support it
-        iS_array = iS_array.flip(dims=(2,))
-        
-        if attributions_fn is not None:
-            return attributions_fn(iS_array)
-        else: return iS_array
-    
+            
     def attribute(
-        self, inputs, additional_forward_args, 
-        attributions_fn=None
-    ):
-        if type(inputs) == tuple:
-            return self._attribute_tuple(
-                inputs, additional_forward_args, attributions_fn
-            )
-        else:
-            return self._attribute(
-                inputs, additional_forward_args, attributions_fn
-            )
-            
-    def _attribute_tuple(
-        self, inputs, additional_forward_args, 
+        self, inputs:Union[torch.Tensor , Tuple[torch.Tensor]], 
+        additional_forward_args: Tuple[torch.Tensor], 
         attributions_fn=None
     ):
         model = self.model
-        with torch.no_grad():
-            if type(additional_forward_args) == tuple:
-                y_original = model(*inputs, *additional_forward_args)
-            else:
-                y_original = model(*inputs, additional_forward_args)
-            
-        y_original = self.format_output(y_original)
+        if type(inputs) != tuple:
+            inputs = tuple([inputs])
             
         attr = []
-        for input_index in range(len(inputs)):
-            batch_size, seq_len, n_features = inputs[input_index].shape
+        with torch.no_grad():
+            y_original = self.format_output(
+                model(*inputs, *additional_forward_args)
+            ) 
             
-            # batch_size, n_output, seq_len, n_features
-            iS_array = torch.zeros(size=(
-                batch_size, y_original.shape[1], seq_len, n_features
-            ), device=inputs[input_index].device)
+            for input_index in range(len(inputs)):
+                batch_size, seq_len, n_features = inputs[input_index].shape
+                
+                # batch_size, n_output, seq_len, n_features
+                iS_array = torch.zeros(size=(
+                    batch_size, y_original.shape[1], seq_len, n_features
+                ), device=inputs[input_index].device)
             
-            with torch.no_grad():
                 for feature in range(n_features):
+                    #TODO: do without cloning
                     cloned = inputs[input_index].clone()
+                    
+                    #TODO: use baselines
                     counterfactuals = self.generate_counterfactuals(
                         batch_size, input_index, feature
                     )
@@ -188,14 +117,9 @@ class WinIT:
                             if i == input_index: inputs_hat.append(cloned)
                             else: inputs_hat.append(inputs[i])
                         
-                        if type(additional_forward_args) == tuple:
-                            y_perturbed = self.format_output(
-                                model(*tuple(inputs_hat), *additional_forward_args)
-                            )
-                        else:
-                            y_perturbed = self.format_output(
-                                model(*tuple(inputs_hat), additional_forward_args)
-                            )
+                        y_perturbed = self.format_output(
+                            model(*tuple(inputs_hat), *additional_forward_args)
+                        )
                         
                         iSab = self._compute_metric(y_original, y_perturbed)
                         iSab = torch.clip(iSab, -1e6, 1e6)
@@ -203,18 +127,20 @@ class WinIT:
                         
                         del y_perturbed, inputs_hat
                     del cloned, counterfactuals
+                    gc.collect()
             
-            # batch_size, n_output, seq_len, n_features        
-            iS_array[:, :, 1:] -= iS_array[:, :, :-1] 
-            
-            # reverse order along the time axis
-            iS_array = iS_array.flip(dims=(2,))
-            
-            if attributions_fn is not None:
-                attr.append(attributions_fn(iS_array))
-            else: attr.append(iS_array)
-            
-        return tuple(attr)
+                # batch_size, n_output, seq_len, n_features        
+                iS_array[:, :, 1:] -= iS_array[:, :, :-1] 
+                
+                # reverse order along the time axis
+                iS_array = iS_array.flip(dims=(2,))
+                
+                if attributions_fn is not None:
+                    attr.append(attributions_fn(iS_array))
+                else: attr.append(iS_array)
+           
+        if len(attr) == 1: return attr[0]
+        else: return tuple(attr)
     
     def get_name(self):
         return 'WinIT'
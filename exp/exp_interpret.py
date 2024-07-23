fix gradient methods for SegRNN and iTransformer

exp/exp_interpret.py
@@ -74,7 +74,8 @@ class Exp_Interpret:
     def initialize_explainer(
         name, model, args, device, dataloader
     ):
-        if name == 'deep_lift':
+        # RuntimeError: cudnn RNN backward can only be called in training mode
+        if name == 'deep_lift' or ('gradient' in name and 'RNN' in args.model):
             # torch.backends.cudnn.enabled=False
             clone = copy.deepcopy(model)
             clone.train() # deep lift moedl needs to be in training mode

models/iTransformer.py
@@ -53,7 +53,10 @@ class Model(nn.Module):
         means = x_enc.mean(1, keepdim=True).detach()
         x_enc = x_enc - means
         stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
-        x_enc /= stdev
+        
+        # FIX: RuntimeError: one of the variables needed for gradient computation has been modified by an inplace operation
+        # previousbly x_enc /= stdev, which performs inplace operation on x_enc
+        x_enc = x_enc / stdev
 
         _, _, N = x_enc.shape
 
@@ -72,7 +75,7 @@ class Model(nn.Module):
         means = x_enc.mean(1, keepdim=True).detach()
         x_enc = x_enc - means
         stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
-        x_enc /= stdev
+        x_enc = x_enc / stdev
 
         _, L, N = x_enc.shape
 
@@ -91,7 +94,7 @@ class Model(nn.Module):
         means = x_enc.mean(1, keepdim=True).detach()
         x_enc = x_enc - means
         stdev = torch.sqrt(torch.var(x_enc, dim=1, keepdim=True, unbiased=False) + 1e-5)
-        x_enc /= stdev
+        x_enc = x_enc / stdev
 
         _, L, N = x_enc.shape
 